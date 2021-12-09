c7d163d2b2857cfdf3cf753651ef72ef6e79eeda
zzw55486402 <55486402@qq.com>
2021-12-09 19:54:59 +0800

    feat: data add

---
+++ data/data.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import csv
+
+
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+ 
+import pandas as pd
+import csv
+ 
+# 读取三元组文件
+h_r_t_name = [":START_ID", ":END_ID", "role"]
+h_r_t = pd.read_table("tuple.txt", sep=' ', names=h_r_t_name)
+print(h_r_t.info())
+print(h_r_t.head())
+
+flag = {}
+file = open("entity_flag_math.txt", "r")
+lines = file.readlines()
+for line in lines:
+    line = line.strip('\n')
+    strs = line.split(sep=" ")
+    if "PRIN" in strs[1]:
+        flag[strs[0]] = "PRIN"
+    elif "MAT" in strs[1]:
+        flag[strs[0]] = "MAT"
+    elif "KNOW" in strs[1]:
+        flag[strs[0]] = "KNOW"
+    # elif "Tree" in strs[1]:
+    #     flag[strs[0]] = "Tree"
+    # else:
+    #     flag[strs[0]] = "Linear"
+
+# 去除重复实体
+entity = set()
+entity_h = h_r_t[':START_ID'].tolist()
+entity_t = h_r_t[':END_ID'].tolist()
+for i in entity_h:
+    entity.add(i)
+for i in entity_t:
+    entity.add(i)
+# 保存节点文件
+csvf_entity = open("entity_math.csv", "w", newline='', encoding='utf-8')
+w_entity = csv.writer(csvf_entity)
+# 实体ID，要求唯一，名称，LABEL标签，可自己不同设定对应的标签
+w_entity.writerow(("entity:ID", "name", ":LABEL"))
+entity = list(entity)
+entity_dict = {}
+
+for i in range(len(entity)):
+    w_entity.writerow(("e" + str(i), entity[i], flag[entity[i]]))
+    entity_dict[entity[i]] = "e"+str(i)
+csvf_entity.close()
+# 生成关系文件，起始实体ID，终点实体ID，要求与实体文件中ID对应，:TYPE即为关系
+h_r_t[':START_ID'] = h_r_t[':START_ID'].map(entity_dict)
+h_r_t[':END_ID'] = h_r_t[':END_ID'].map(entity_dict)
+h_r_t[":TYPE"] = h_r_t['role']
+h_r_t.pop('role')
+h_r_t.to_csv("roles_math.csv", index=False)
